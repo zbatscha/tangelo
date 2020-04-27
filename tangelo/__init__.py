d596be9d9fc091a776edd2562799bc8e281771f3
Ziv Batscha <zbatscha@princeton.edu>
2020-04-27 15:25:54 -0700

    update debug mode

--- tangelo/__init__.py
+++ tangelo/__init__.py
@@ -23,6 +23,7 @@ log = logging.getLogger('tangelo')
 export POSTGRES variables and SECRET_KEY key to environment and remove!
 """
 
+app.config['DEBUG'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
